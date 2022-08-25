024993ab767b1075ab5565512a826b617941c97c
Timur <timur.kokumbaev@gmail.com>
2022-08-25 19:27:47 +0200

    Burger menu mit JS hinzugefügt

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.burger-menu');
 
 menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active-burger-menu');
     menu.classList.toggle('active-burger-menu');
-})
+});
 
-console.log(menu);

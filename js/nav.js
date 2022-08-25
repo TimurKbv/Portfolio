40c81c9c4c734d09587aa0e4b4ee381091ce1030
Timur <timur.kokumbaev@gmail.com>
2022-08-25 14:36:13 +0200

    files index.html & style.css unbenannt

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.burger-menu');
+
+menuBtn.addEventListener('click', function () {
+    menu.classList.toggle('active-burger-menu');
+})
+
+console.log(menu);
